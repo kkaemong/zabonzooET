cee032e2dec0dd9abe304ff44fb3be5be3172004
kimhaerin <cyberkhl@naver.com>
2026-02-25 13:13:12 +0900

    init: 백엔드 프로젝트 초기 설정

---
+++ BE/src/test/java/com/ssafy/amagetdon/AmagetdonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.amagetdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmagetdonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
